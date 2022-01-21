44f563c0f68b50c016ea7de708c3d9e6ed64878b
Akash Patil <apatil@tcd.ie>
2022-01-21 16:42:45 +0000

    Spring Boot project initial push

---
+++ scm/src/test/java/com/tcd/ase/scm/ScmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcd.ase.scm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
