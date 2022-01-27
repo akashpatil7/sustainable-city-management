2e40b628da61632227d19d4a4b9964f58027098e
Sophie Crowley <crowleso@tcd.ie>
2022-01-27 17:03:56 +0000

    Replacing lombok with getters/setters

--- user-service/src/main/java/com/tcd/ase/userservice/entity/User.java
+++ user-service/src/main/java/com/tcd/ase/userservice/entity/User.java
@@ -7,10 +7,26 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document("Users")
-@Getter
-@Setter
 public class User {
 	private String userName;
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 	@Id
 	private String userEmail;
 	private String password;
--- user-service/src/main/java/com/tcd/ase/userservice/models/UserRegistrationRequest.java
+++ user-service/src/main/java/com/tcd/ase/userservice/models/UserRegistrationRequest.java
@@ -1,13 +1,26 @@
 package com.tcd.ase.userservice.models;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public class UserRegistrationRequest {
 
 	private String username;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 	private String email;
 	private String password;
 }
