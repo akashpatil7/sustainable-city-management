cfbf6e3a51ebaeaf6e15f00cb0a01a76b9ecd055
Akul Rastogi <akulrastogi92@gmail.com>
2022-01-27 18:23:16 +0000

    update

--- trend-analysis/src/main/java/com/tcd/ase/trendsengine/controller/TrendsEngineControllerImpl.java
+++ trend-analysis/src/main/java/com/tcd/ase/trendsengine/controller/TrendsEngineControllerImpl.java
@@ -2,6 +2,7 @@ package com.tcd.ase.trendsengine.controller;
 
 import com.tcd.ase.utils.JWTokenHelper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.CrossOrigin;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +11,16 @@ import com.tcd.ase.trendsengine.repository.TrendsRepository;
 import com.tcd.ase.trendsengine.models.TrendsRequest;
 
 import java.util.Optional;
-import org.json.JSONObject;
+import java.io.FileReader;
+import java.util.Iterator;
+import java.net.SocketPermission;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 @RestController
+@CrossOrigin
 public class TrendsEngineControllerImpl implements TrendsEngineController {
 	
 	@Autowired
@@ -26,6 +34,27 @@ public class TrendsEngineControllerImpl implements TrendsEngineController {
 		repository.save(user);
 		response = new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
 		return response;*/
+
+		JSONParser parser = new JSONParser();
+		System.out.println("Working Directory = " + System.getProperty("user.dir"));
+
+		try {
+			JSONArray Jan25 = (JSONArray) parser.parse(new FileReader("src/main/resources/20220125141500.json"));
+			JSONArray Jan27 = (JSONArray) parser.parse(new FileReader("src/main/resources/20220127163500.json"));
+
+			// Iterator<JSONObject> iteratorJan25 = Jan25.iterator();
+			// Iterator<JSONObject> iteratorJan27 = Jan27.iterator();
+			// while (iteratorJan25.hasNext() && iteratorJan27.hasNext()) {
+			// 	System.out.println(iterator.next());
+			// }
+
+			//System.out.println(Jan25.get(0).get("bike_stands"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+
+
 
 		JSONObject obj = new JSONObject();
 
