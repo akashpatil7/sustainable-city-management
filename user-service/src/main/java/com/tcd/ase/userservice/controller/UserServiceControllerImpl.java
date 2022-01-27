d38ae02d32761a00a0ecff689bbddb1375cdb28f
Akash Patil <apatil@tcd.ie>
2022-01-27 13:15:09 +0000

    G81 - Added lombok annotations + code refactor

--- user-service/src/main/java/com/tcd/ase/userservice/controller/UserServiceControllerImpl.java
+++ user-service/src/main/java/com/tcd/ase/userservice/controller/UserServiceControllerImpl.java
@@ -1,18 +1,17 @@
 package com.tcd.ase.userservice.controller;
 
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.tcd.ase.userservice.entity.User;
 import com.tcd.ase.userservice.models.UserLoginRequest;
 import com.tcd.ase.userservice.models.UserRegistrationRequest;
 import com.tcd.ase.userservice.repository.UserRepository;
 import com.tcd.ase.userservice.service.UserMapper;
 import com.tcd.ase.utils.JWTokenHelper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 public class UserServiceControllerImpl implements UserServiceController {
@@ -26,7 +25,7 @@ public class UserServiceControllerImpl implements UserServiceController {
 		final ResponseEntity<String> response;
 		final String token;
 		Optional<User> user = repository.findById(request.getEmail());
-		if(user != null) {
+		if(user.isPresent()) {
 			return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Invalid username or password");
 		}
 		else {
@@ -34,7 +33,7 @@ public class UserServiceControllerImpl implements UserServiceController {
 			if(!request.getPassword().equals(ruser.getPassword())) {
 				return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Invalid username or password");
 			}
-			 token = helper.generateToken(ruser.getUser_name());
+			 token = helper.generateToken(ruser.getUserName());
 			return ResponseEntity.status(HttpStatus.OK).body(token);
 		}
 		
--- user-service/src/main/java/com/tcd/ase/userservice/models/UserRegistrationRequest.java
+++ user-service/src/main/java/com/tcd/ase/userservice/models/UserRegistrationRequest.java
@@ -1,27 +1,13 @@
 package com.tcd.ase.userservice.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UserRegistrationRequest {
 
 	private String username;
 	private String email;
 	private String password;
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
 }
