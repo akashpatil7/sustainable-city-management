0de44e27e794a802e014c568aa7a3014710c9cf8
Sophie Crowley <crowleso@tcd.ie>
2022-01-27 17:04:45 +0000

    Update User.java

--- user-service/src/main/java/com/tcd/ase/userservice/entity/User.java
+++ user-service/src/main/java/com/tcd/ase/userservice/entity/User.java
@@ -1,8 +1,5 @@
 package com.tcd.ase.userservice.entity;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
