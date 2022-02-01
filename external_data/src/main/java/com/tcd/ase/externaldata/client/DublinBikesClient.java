be13d4d38727d848d56e566f3173309a6408d6a9
SheetalRaut <msheetalraut@gmail.com>
2022-02-01 12:54:40 +0000

    story4 : resolved merge conflict

--- external_data/src/main/java/com/tcd/ase/externaldata/client/DublinBikesClient.java
+++ external_data/src/main/java/com/tcd/ase/externaldata/client/DublinBikesClient.java
@@ -3,6 +3,7 @@ package com.tcd.ase.externaldata.client;
 import com.tcd.ase.externaldata.service.ProcessDublinBikesDataService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
