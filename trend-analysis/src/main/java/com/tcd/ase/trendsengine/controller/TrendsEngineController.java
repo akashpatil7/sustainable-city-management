2e204e36fa07aa723dc9d5663a980bf6fc5d6463
Akul Rastogi <akulrastogi92@gmail.com>
2022-02-01 14:09:20 +0000

    java fetch averages from pymongo implemented

--- trend-analysis/src/main/java/com/tcd/ase/trendsengine/controller/TrendsEngineController.java
+++ trend-analysis/src/main/java/com/tcd/ase/trendsengine/controller/TrendsEngineController.java
@@ -1,7 +1,7 @@
 package com.tcd.ase.trendsengine.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import com.tcd.ase.trendsengine.models.TrendsRequest;
@@ -15,8 +15,8 @@ public interface TrendsEngineController {
 
 	String BASE_URL = "/trends";
 
-	@PostMapping(path = BASE_URL + "/getTrendsData")
-	public ResponseEntity<String> getTrendsData(@RequestBody TrendsRequest request);
+	@GetMapping(path = BASE_URL + "/getTrendsData")
+	public ResponseEntity<String> getTrendsData();
 
 	//@PostMapping(path = BASE_URL + "/login")
 	//public ResponseEntity<String> login(@RequestBody TrendsRequest request);
--- trend-analysis/src/main/java/com/tcd/ase/trendsengine/controller/TrendsEngineControllerImpl.java
+++ trend-analysis/src/main/java/com/tcd/ase/trendsengine/controller/TrendsEngineControllerImpl.java
@@ -1,80 +1,48 @@
 package com.tcd.ase.trendsengine.controller;
 
-import com.tcd.ase.utils.JWTokenHelper;
-import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.tcd.ase.trendsengine.repository.TrendsRepository;
-import com.tcd.ase.trendsengine.models.TrendsRequest;
+//import com.tcd.ase.trendsengine.repository.TrendsRepository;
 
-import java.io.FileReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.io.OutputStream;
-import java.io.InputStreamReader;
-import java.io.BufferedReader;
-import org.json.simple.JSONArray;
-import org.json.simple.parser.JSONParser;
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 @RestController
 public class TrendsEngineControllerImpl implements TrendsEngineController {
 	
-	@Autowired
-	private TrendsRepository repository;
+	//@Autowired
+	//private TrendsRepository repository;
 
 	@Override
-	public ResponseEntity<String> getTrendsData(TrendsRequest request) {
-		/*UserMapper mapper = new UserMapper();
-		ResponseEntity<Void> response = null;
-		Trends user = mapper.fromRegistrationRequestToEntity(request);
-		repository.save(user);
-		response = new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
-		return response;*/
-		JSONParser parser = new JSONParser();
+	public ResponseEntity<String> getTrendsData() {
 
+		System.out.println("getTrendsData called!!");
+		System.out.println("Setting up the client");
+		OkHttpClient client = new OkHttpClient().newBuilder().build();
+		
+		System.out.println("Setting up the request");
+		Request request = new Request.Builder()
+			.url("http://localhost:8050/getDailyAverages")
+			.method("GET", null)
+			.build();
+		Response response;
 		try {
-			URL url = new URL ("http://192.168.0.94:8050/getDailyAverages");
-			HttpURLConnection con = (HttpURLConnection)url.openConnection();
-			con.setRequestMethod("POST");
-			con.setRequestProperty("Content-Type", "application/json; utf-8");
-			con.setRequestProperty("Accept", "application/json");
-			con.setDoOutput(true);
+			System.out.println("Trying to get response");
+			response = client.newCall(request).execute();
 
-			JSONArray Jan25 = (JSONArray) parser.parse(new FileReader("src/main/resources/20220125141500.json"));
-			JSONArray Jan27 = (JSONArray) parser.parse(new FileReader("src/main/resources/20220127163500.json"));
-
-			String jsonInputString = "{ \"1\" : " + Jan25.toString() + ", \"2\" : " + Jan27.toString() + "}";
-
-			try(OutputStream os = con.getOutputStream()){
-				byte[] input = jsonInputString.getBytes("utf-8");
-				os.write(input, 0, input.length);			
-			}
-	
-			int code = con.getResponseCode();
-			System.out.println(code);
-			
-			try(BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), "utf-8"))){
-				StringBuilder response = new StringBuilder();
-				String responseLine = null;
-				while ((responseLine = br.readLine()) != null) {
-					response.append(responseLine.trim());
-				}
-				//System.out.println(response.toString());
-				return ResponseEntity.status(HttpStatus.OK).body(response.toString());
-			}
-
-		} catch (MalformedURLException e1) {
-			e1.printStackTrace();
-		} catch (Exception e) {
+			System.out.println("Returning response");
+			return ResponseEntity.status(HttpStatus.OK).body(response.body().string());
+		} catch (IOException e) {
+			System.out.println("ERROR");
 			e.printStackTrace();
 		}
-		
-		System.out.println("getTrendsData - TrendsEngineControllerImp");
-		return ResponseEntity.status(HttpStatus.OK).body("");
+		System.out.println("FAILED");
+		return ResponseEntity.status(HttpStatus.FAILED_DEPENDENCY).body("fail");	
 	}
-
-
 }
