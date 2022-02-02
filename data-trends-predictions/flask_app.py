6977071c6bac245576242a5cb2d5fd3f0c5e0f4f
Akash Patil <apatil@tcd.ie>
2022-02-02 15:36:37 +0000

    Seperated data blocks for getRecommendations

--- data-trends-predictions/flask_app.py
+++ data-trends-predictions/flask_app.py
@@ -1,14 +1,14 @@
 from flask import Flask, request
 import pandas as pd
-import py_eureka_client.eureka_client as eureka_client
+#import py_eureka_client.eureka_client as eureka_client
 from pymongo import MongoClient
 from datetime import datetime
 from bson.json_util import dumps
 
 rest_port = 8050
-eureka_client.init(eureka_server="http://localhost:8761/eureka",
-                   app_name="py-data-trends-predictions-service",
-                   instance_port=rest_port)
+#eureka_client.init(eureka_server="http://localhost:8761/eureka",
+#                   app_name="py-data-trends-predictions-service",
+#                   instance_port=rest_port)
 client = MongoClient("mongodb+srv://admin:admin@cluster1.varva.mongodb.net/city_dashboard")
 app = Flask(__name__)
 
@@ -65,9 +65,7 @@ def getRecommendations():
 	).sort([("harvestTime", -1), ("availableBikeStands", -1)]).limit(5)
 
 	print("Calculating recommendations")
-	df1 = pd.DataFrame(list(mostEmptyBikeStationData))
-	df2 = pd.DataFrame(list(mostAvailableBikeStationData))
-	return df1.append(df2).to_json(orient = 'records')
+	return dumps(list(mostEmptyBikeStationData)) + dumps(list(mostAvailableBikeStationData))
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port = rest_port)
