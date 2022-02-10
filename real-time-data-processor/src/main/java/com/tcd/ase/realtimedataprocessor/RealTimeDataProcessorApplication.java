3cbe2db6bdee91aac1d8a55f621982543f96aa98
Akash Patil <apatil@tcd.ie>
2022-02-10 17:11:21 +0000

    Created new microservice - real time data processor

---
+++ real-time-data-processor/src/main/java/com/tcd/ase/realtimedataprocessor/RealTimeDataProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.tcd.ase.realtimedataprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeDataProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeDataProcessorApplication.class, args);
+	}
+
+}
---
+++ real-time-data-processor/src/test/java/com/tcd/ase/realtimedataprocessor/RealTimeDataProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcd.ase.realtimedataprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeDataProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
