59a98f3d2909aabb53c85d828242fa5395a44c27
Sophie Crowley <crowleso@tcd.ie>
2022-02-10 16:29:58 +0000

    Move into proper directory

--- data-trends-predictions/flask_app.py
+++ data-trends-predictions/flask_app.py
@@ -88,4 +88,3 @@ class Analysis():
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=rest_port)
-
--- data-trends-predictions/test_flask_app.py
+++ data-trends-predictions/test_flask_app.py
@@ -1,9 +1,12 @@
 import unittest
 from flask_app import Analysis
 
+# TODO(all): Write legitimate tests for flask_app.py.
+
 class AnalysisTests(unittest.TestCase):
     def testCurrentHourAverages(self):
         analysis = Analysis()
         hourAverages = analysis.getCurrentHourAverages()
         testHourAverages = analysis.getCurrentHourAverages()
         self.assertEqual(hourAverages, testHourAverages)
+        
