12e8a804dfc38401cec9b68da4b1fe260228b109
Akul Rastogi <akulrastogi92@gmail.com>
2022-02-18 16:07:52 +0000

    added code for getting stops data

--- external_data/src/main/java/com/tcd/ase/externaldata/service/impl/ProcessDublinBusDataServiceImpl.java
+++ external_data/src/main/java/com/tcd/ase/externaldata/service/impl/ProcessDublinBusDataServiceImpl.java
@@ -1,20 +1,17 @@
 package com.tcd.ase.externaldata.service.impl;
 
-import com.google.gson.Gson;
-import com.google.gson.internal.LinkedTreeMap;
-import com.tcd.ase.externaldata.entity.DublinBusStopSequence;
-import com.tcd.ase.externaldata.entity.DublinCityBusRoutes;
-import com.tcd.ase.externaldata.model.DublinBike;
+import com.mongodb.client.model.Filters;
+import com.tcd.ase.externaldata.entity.DublinBusHistorical;
+import com.tcd.ase.externaldata.entity.DublinBusHistoricalStopSequence;
+import com.tcd.ase.externaldata.entity.DublinBusStops;
 import com.tcd.ase.externaldata.model.DublinBus;
-import com.tcd.ase.externaldata.model.dublinBus.Entity;
-import com.tcd.ase.externaldata.model.dublinBus.Header;
-import com.tcd.ase.externaldata.model.dublinBus.Trip;
-import com.tcd.ase.externaldata.repository.DublinBusStopSequenceRepository;
-import com.tcd.ase.externaldata.repository.DublinCityBusRoutesRepository;
+import com.tcd.ase.externaldata.model.bus.Entity;
+import com.tcd.ase.externaldata.model.bus.Trip;
+import com.tcd.ase.externaldata.repository.bus.DublinBusHistoricalRepository;
+import com.tcd.ase.externaldata.repository.bus.DublinBusStopsRepository;
+import com.tcd.ase.externaldata.repository.bus.DublinBusRoutesRepository;
 import com.tcd.ase.externaldata.service.ProcessDublinBusDataService;
-import org.codehaus.jettison.json.JSONArray;
-import org.codehaus.jettison.json.JSONException;
-import org.codehaus.jettison.json.JSONObject;
+import org.bson.conversions.Bson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -25,8 +22,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 
-import java.util.Collections;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -35,44 +30,54 @@ import java.util.stream.Collectors;
 public class ProcessDublinBusDataServiceImpl implements ProcessDublinBusDataService {
 
     @Autowired
-    private DublinCityBusRoutesRepository dublinCityBusRoutesRepository;
+    private DublinBusRoutesRepository dublinBusRoutesRepository;
 
     @Autowired
-    private DublinBusStopSequenceRepository dublinBusStopSequenceRepository;
+    private DublinBusStopsRepository dublinBusStopsRepository;
+
+    @Autowired
+    private DublinBusHistoricalRepository dublinBusHistoricalRepository;
 
     @Override
     @Scheduled(fixedRate = 10000)
     public void processData() {
-        //System.out.println(data);
-        Set<String> dublinCityBusRoutesList = dublinCityBusRoutesRepository.findAll().stream().map(i -> i.getRoute_id()).collect(Collectors.toSet());
 
-        //LinkedTreeMap dublinBus = (LinkedTreeMap) new Gson().fromJson(data, Object.class);
-        //Header header = new Gson().fromJson(dublinBus.get("Header").toString(), Header.class);
+        Set<String> dublinCityBusRouteIdsList = dublinBusRoutesRepository.findAll()
+                                                .stream()
+                                                .map(i -> i.getRoute_id())
+                                                .collect(Collectors.toSet());
+
 
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.set("x-api-key","2de8ef2694a24e1c82fa92aaeba7d351");
         HttpEntity<Void> httpEntity = new HttpEntity<>(headers);
-        ResponseEntity<DublinBus> dublinBusResponseEntity = restTemplate.exchange("https://api.nationaltransport.ie/gtfsr/v1?format=json", HttpMethod.GET, httpEntity, DublinBus.class, 1);
+        ResponseEntity<DublinBus> dublinBusResponseEntity =
+                restTemplate.exchange("https://api.nationaltransport.ie/gtfsr/v1?format=json", HttpMethod.GET, httpEntity, DublinBus.class, 1);
         List<Entity> entities = dublinBusResponseEntity.getBody().getEntity()
                                 .stream()
-                                .filter(x -> dublinCityBusRoutesList.contains(x.getTripUpdate().getTrip().getRouteId()))
+                                .filter(x -> dublinCityBusRouteIdsList
+                                            .contains(x.getTripUpdate().getTrip().getRouteId()))
                                 .collect(Collectors.toList());
         saveToDB(entities);
         System.out.println("Processing completed");
     }
 
     void saveToDB(List<Entity> entities) {
+
+        List<DublinBusStops> DublinBusStopsList = dublinBusStopsRepository.findAll();
+
         for (Entity busEntity: entities) {
             Trip trip = busEntity.getTripUpdate().getTrip();
-            DublinBusStopSequence dublinBusStopSequence = new DublinBusStopSequence.DublinBusStopSequenceBuilder()
+            DublinBusHistorical dublinBusHistorical = new DublinBusHistorical.DublinBusHistoricalBuilder()
                                                                                 .withTripId(trip.getTripId())
                                                                                 .withRouteId(trip.getRouteId())
                                                                                 .withStartTimestamp(trip.getStartTime()) //please ignore this I'm still working on timestamp
                                                                                 .withScheduleRelationship(trip.getScheduleRelationship())
                                                                                 .withStopTimeUpdate(busEntity.getTripUpdate().getStopTimeUpdate())
                                                                                 .build();
-            //dublinBusStopSequenceRepository.save(dublinBusStopSequence);
+            Bson filter = Filters.and(Filters.eq("qty", 5), Filters.ne("color", "pink"));
+            dublinBusHistoricalRepository.save(dublinBusHistorical);
 
         }
     }
