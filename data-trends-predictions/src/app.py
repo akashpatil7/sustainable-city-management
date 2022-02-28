feda23e58bbd16fa6c99d6c4e1ca3ac72c3cfb93
Akul Rastogi <akulrastogi92@gmail.com>
2022-02-28 00:26:54 +0000

    Added comments and updated for config variables

--- data-trends-predictions/src/app.py
+++ data-trends-predictions/src/app.py
@@ -2,30 +2,38 @@ from flask import Flask
 from flask_restful import Api
 from flask_cors import CORS
 from pymongo import MongoClient
-import argparse
 import py_eureka_client.eureka_client as eureka_client
 
 from src.resources.trends import Trends
 from src.resources.recommendations import Recommendations
 
-#parser = argparse.ArgumentParser()
-#parser.add_argument('--env', help='Evironment in which to run', type=str)
-#args = parser.parse_args()
-host = "localhost"
-rest_port = 8050
-#if args.env:
-    #host = args.env
-
-eureka_client.init(eureka_server=f'http://{host}:8761/eureka',
-                   app_name="trends",
-                   instance_port=rest_port)
-
+# INIT Flask App
 app = Flask(__name__)
+
+# INIT Configs
+app.config.from_pyfile('settings.py')
+DB_CONNECTION_STRING = app.config.get('DB_CONNECTION_STRING')
+FLASK_RUN_PORT = int(app.config.get('FLASK_RUN_PORT'))
+EUREKA_SERVER_HOST = app.config.get('EUREKA_SERVER_HOST')
+EUREKA_REGISTERED_APP_NAME = app.config.get('EUREKA_REGISTERED_APP_NAME')
+
+EUREKA_SERVER_NAME = f'http://{EUREKA_SERVER_HOST}:8761/eureka'
+
+# INIT Eureka Client
+eureka_client.init(eureka_server = EUREKA_SERVER_NAME,
+                   app_name = EUREKA_REGISTERED_APP_NAME,
+                   instance_port = FLASK_RUN_PORT)
+
+# INIT Cors
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
+
+# INIT RESTFul API 
 api = Api(app)
 
-client = MongoClient("mongodb+srv://admin:admin@cluster1.varva.mongodb.net/city_dashboard")
+# INIT MongoClient 
+client = MongoClient(DB_CONNECTION_STRING)
 client_as_args = {'client': client}
 
+# ADD Routes
 api.add_resource(Trends, '/trends/<string:data_indicator>/<string:action>', resource_class_kwargs=client_as_args)
 api.add_resource(Recommendations, '/recommendations/<string:data_indicator>/<string:action>', resource_class_kwargs=client_as_args)
