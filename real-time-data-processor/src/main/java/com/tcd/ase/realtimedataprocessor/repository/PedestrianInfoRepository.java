b8bb2a52f0907d3f2bf98947b7fc9d6b8a41f5eb
Sophie Crowley <crowleso@tcd.ie>
2022-03-03 15:37:39 +0000

    Fix

--- real-time-data-processor/src/main/java/com/tcd/ase/realtimedataprocessor/repository/PedestrianInfoRepository.java
+++ real-time-data-processor/src/main/java/com/tcd/ase/realtimedataprocessor/repository/PedestrianInfoRepository.java
@@ -5,11 +5,11 @@ import com.tcd.ase.realtimedataprocessor.entity.PedestrianInfoDAO;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.List;
 
 @Repository
 public interface PedestrianInfoRepository extends MongoRepository<PedestrianInfoDAO, Integer> {
 
-    Optional<PedestrianInfoDAO> findFirstByOrderByTimeDesc();
+    List<PedestrianInfoDAO> findAll();
 
 }
