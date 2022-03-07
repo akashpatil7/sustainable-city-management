a303ea83a4578809b1e87b5683b4afbd09c80e01
Akul Rastogi <akulrastogi92@gmail.com>
2022-03-07 15:28:18 +0000

    testing updates: added pytest (w/ client fixture by moving flask into application factory)

--- data-trends-predictions/test_data_app.py
+++ data-trends-predictions/test_data_app.py
@@ -1,12 +1,15 @@
-import unittest
-from src.app import app
+import pytest
+from src.app import create_app
 from src.resources.trends.bike import EndPointMethods as BikeTrendsEndPoints
 
-class AnalysisTests(unittest.TestCase):
-	def testBikeTrendsEndPoints(self):
-		for end_point_name, member in BikeTrendsEndPoints.__members__.items():
-			print("[TEST] Bike Trends End Point: " + end_point_name)
-			response = app.test_client().get('trends/bike/' + end_point_name)
-			assert response.status_code == 200
+@pytest.fixture
+def client(monkeypatch):
+	monkeypatch.setenv('FLASK_RUN_PORT', '8050')
+	client = create_app().test_client()
+	yield client
 
-	
+def test_bike_trends_end_points(client):
+	for end_point_name, member in BikeTrendsEndPoints.__members__.items():
+		print("[TEST] Bike Trends End Point: " + end_point_name)
+		response = client.get('trends/bike/' + end_point_name)
+		assert response.status_code == 200
