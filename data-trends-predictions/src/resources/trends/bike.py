7eb675eb26901bac15f567f8bfe196f1c4017b06
Akul Rastogi <akulrastogi92@gmail.com>
2022-03-07 11:51:00 +0000

    PY refactoring for DB and API response layer + added new APIs for popularity

--- data-trends-predictions/src/resources/trends/bike.py
+++ data-trends-predictions/src/resources/trends/bike.py
@@ -1,64 +1,54 @@
 from datetime import datetime
-from bson.json_util import dumps
-from flask import make_response
+from src.common.response import Response
+from enum import Enum
 
+class End_Point_Methods(Enum):
+	getCurrentHourAverages = "get_current_hour_averages"
+	getHourlyAverageForAllStation = "get_hourly_average_for_all_station"
+	getPopularityAverageHistorical = "get_popularity_average_historical"
+	getPopularityAverageToday = "get_popularity_average_today"
 
 class Bike():
-	def __init__(self, client):
-		self.client = client
+	def __init__(self, db):
+		self.db = db
 		print("Initiating Bike Trends")
 
 
 	def perform_action(self, action):
-		print("Performing: " + action)
-		if (action == "getCurrentHourAverages"):
-			return self.get_current_hour_averages()
-		elif (action == "getHourlyAverageForAllStation"):
-			return self.get_hourly_average_for_all_station()
-		elif (action == "getHistoricalPopularityAverage"):
-			return self.get_popularity_average_historical()
-
+		try:
+			return getattr(self, End_Point_Methods[action].value)()
+		except KeyError:
+			print("[Bike Trends] EndPoint not found")
+		except AttributeError:
+			print("[Bike Trends] EndPoint cannot be resolved")
 		return "trends bike: " + action + " not found"
 
 
 	def get_current_hour_averages(self):
 		print("[Bike Trends] get current hour averages")
-		avg_hourly_availability = self.client.city_dashboard.Bike_AvgHourlyAvailability
 
 		hour_filter = {'_id.hour': datetime.now().hour}
 		print("Filter: ", hour_filter)
 
-		data = avg_hourly_availability.find(filter=hour_filter)
-		self.send_response(data)
+		data = self.db.get_view("Bike_AvgHourlyAvailability", hour_filter)
+		return Response.send_json_response_200(data)
 
 
 	def get_hourly_average_for_all_station(self):
 		print("[Bike Trends] get hourly average for all station")
-		self.fetch_data_send_response("Bike_AvgHourlyAvailability")
-		# avg_hourly_availability = self.client.city_dashboard.Bike_AvgHourlyAvailability
-
-		# data = avg_hourly_availability.find()
-		# self.send_response(data)
+		return self.fetch_view_send_response("Bike_AvgHourlyAvailability")
 
 
 	def get_popularity_average_historical(self):
 		print("[Bike Trends] get popularity average for all station historical")
-		self.fetch_data_send_response("Bike_PopularityAverageHistorical")
-		# popularity_average_historical = self.client.city_dashboard.Bike_PopularityAverageHistorical
-
-		# data = popularity_average_historical.find()
-		# self.send_response(data)
+		return self.fetch_view_send_response("Bike_PopularityAverageHistorical")
 
 
-	def fetch_data_send_response(self, collection_name):
-		collection = self.client.city_dashboard[collection_name]
-		print(collection)
-		data = collection.find()
-		print(dumps(list(data)))
-		self.send_response(data)
+	def get_popularity_average_today(self):
+		print("[Bike Trends] get popularity average for all station today")
+		return self.fetch_view_send_response("Bike_PopularityAverageToday")
 
 
-	def send_response(self, data):
-		print("Returning data ... ")
-		#return make_response(dumps(list(data)))
-		return (dumps(list(data)))
+	def fetch_view_send_response(self, collection_name):
+		data = self.db.get_view(collection_name)
+		return Response.send_json_response_200(data)
