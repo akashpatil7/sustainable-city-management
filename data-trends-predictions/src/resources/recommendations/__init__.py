2016368ff7d80b4248da7b1bca2dfeaad41a4642
Sophie Crowley <crowleso@tcd.ie>
2022-03-08 14:46:28 +0000

    Add basic Aqi Recommendation

--- data-trends-predictions/src/resources/recommendations/__init__.py
+++ data-trends-predictions/src/resources/recommendations/__init__.py
@@ -1,10 +1,15 @@
 from flask_restful import Resource
 from .bike import Bike
+from .aqi import Aqi
 
 class Recommendations(Resource):
 	def __init__(self, **kwargs):
 		self.db = kwargs['db']
 		self.bike = Bike(self.db)
-	
+		self.aqi = Aqi(self.db)
+
 	def get(self, data_indicator, action):
-		return self.bike.perform_action(action)
+		if data_indicator == 'bike':
+			return self.bike.perform_action(action)
+		if data_indicator == 'aqi':
+			return self.aqi.perform_action(action)
