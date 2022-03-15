eeacd7daa0af08bc3f78f4a8dbab8d1e96ea5eb8
Maddie <13711456+maddiecomtois@users.noreply.github.com>
2022-03-15 13:57:51 +0000

    add bike-pedestrian recommendation
    (not sure if this works yet)
    need to fix Cors error

--- data-trends-predictions/src/resources/recommendations/bike.py
+++ data-trends-predictions/src/resources/recommendations/bike.py
@@ -1,8 +1,10 @@
 from src.common.response import Response
 from enum import Enum
+import math
 
 class EndPointMethods(Enum):
 	getRecommendations = "get_recommendations"
+	getBikePedestrianRecommendations = "get_bike_pedestrian_recommendations"
 
 class Bike():
 	def __init__(self, db):
@@ -56,6 +58,57 @@ class Bike():
 			most_empty_bike_station_data,
 			'mostAvailableBikeStationData':
 			most_available_bike_station_data
+		}
+		return Response.send_json_200(data)
+
+	def get_bike_pedestrian_recommendations(self):
+		print("[Bike-Pedestrian Recommendations] Get")
+		dublin_bikes = self.db.get_collection("Dublin_Bikes")
+		pedestrian = self.db.get_collection("Pedestrian")
+
+		# get 5 most filled stations
+		most_available_bike_station_data = list(
+			dublin_bikes.find({}, {
+				'name': True,
+				'harvestTime': True,
+				'availableBikeStands': True,
+				'bikeStands': True,
+				'availableBikes': True,
+				'_id': False
+			}).sort([("harvestTime", -1),
+						("availableBikeStands", -1)]).limit(5))
+		
+		# get 5 most busy areas
+		highest_count_pedestrian_data = list(
+			ped.find({}, {
+				'count': True,
+				'street': True,
+			}).sort([("count", 1)]).limit(5))
+		
+		# for each busy area, calculuate the closest bike station that is full
+		# recommend sending bikes from closest fullest station to this area	
+		move_bikes_from = []
+		move_bikes_to = []
+		
+		for ped in highest_count_pedestrian_data:
+			closestBikeStandDistance = float('inf');
+			closestBikeStand = None
+
+			for stand in most_available_bike_station_data:
+				distance = math.sqrt((stand['latitude'] - ped['streetLatitude']) ** 2 + (stand['longitude'] - ped['streetLongitude']) ** 2);
+				if distance < closestBikeStandDistance and stand not in move_bikes_from:
+					closestBikeStandDistance = distance
+					closestBikeStand = stand
+			
+			if not stand is None:
+				move_bikes_from.append(stand)
+				move_bikes_to.append(ped)
+
+		data = {
+			'moveBikesFrom':
+			move_bikes_from,
+			'moveBikesTo':
+			move_bikes_to
 		}
 		return Response.send_json_200(data)
 		
