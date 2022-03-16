f1a24420f8755648db9955f82f1bc546866b6030
Ciara Gilsenan <ciaragil98@gmail.com>
2022-03-16 15:57:31 +0000

    predicitve model for pedestrians created

---
+++ predictive-models/app.py
@@ -0,0 +1,44 @@
+from flask import Flask
+from apscheduler.schedulers.background import BackgroundScheduler
+from pymongo import MongoClient
+from model_pedestrians import train_pedestrian_model, get_pedestrian_predictions
+from datetime import datetime
+
+app = Flask(__name__)
+
+sched = BackgroundScheduler(daemon=True)
+
+# SCM db
+eureka_db_string = 'mongodb+srv://admin:admin@cluster1.varva.mongodb.net/city_dashboard'
+client = MongoClient(eureka_db_string)
+eureka_db = client.city_dashboard
+eureka_collection = eureka_db.predictive_models
+
+# test local db
+db = client.test_database
+collection  = db.model_collection
+
+def save_pedestrian_model():
+    ped_model = train_pedestrian_model(eureka_db)
+    date = datetime.now()
+    new_entry = {"$set": {"model": ped_model, "date_of_training": date}}
+
+    info = eureka_collection.update_one({"indicator": "pedestrian"}, new_entry)
+    print("Pedestrian Model saved to DB")
+    print(info)
+
+
+@app.route("/get_pedestrian_predictions", methods=['GET'])
+def load_pedestrian_model():
+    ped_model = eureka_collection.find_one({"indicator": "pedestrian"})
+
+    pred = get_pedestrian_predictions(ped_model['model'])
+    return ''.join(str(p) for p in pred)
+    
+
+if __name__ == "__main__":
+    #sched.add_job(save_pedestrian_model, 'cron', hour='12')
+    sched.add_job(save_pedestrian_model, 'cron', minute='*')
+    sched.start()
+    print('Scheduler Running...')
+    app.run()
