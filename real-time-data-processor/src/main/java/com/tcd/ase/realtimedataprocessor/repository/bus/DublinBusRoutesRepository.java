e3eb882a7f83dccc8467f9633c18b4e40560a7c1
Akul Rastogi <akulrastogi92@gmail.com>
2022-03-22 15:28:24 +0000

    real-time-dashboard removed repository

--- real-time-data-processor/src/main/java/com/tcd/ase/realtimedataprocessor/repository/bus/DublinBusRoutesRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tcd.ase.realtimedataprocessor.repository.bus;
-
-import com.tcd.ase.realtimedataprocessor.entity.DublinCityBusRoutes;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface DublinBusRoutesRepository extends MongoRepository<DublinCityBusRoutes, String > {
-}
