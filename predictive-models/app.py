7b3620a8deba45420f393f5b790ee5aa8629df96
aryanveer <aryanveer310@gmail.com>
2022-03-25 16:24:42 +0000

    dublin bike predictive model using static data for 3 months

--- predictive-models/app.py
+++ predictive-models/app.py
@@ -3,6 +3,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from pymongo import MongoClient
 from model_pedestrians import train_pedestrian_model, get_pedestrian_predictions
 from datetime import datetime
+from bikes_model import create_bikes_model, get_bike_predictions
 
 app = Flask(__name__)
 
@@ -27,6 +28,13 @@ def save_pedestrian_model():
     print("Pedestrian Model saved to DB")
     print(info)
 
+def save_bike_model():
+    bike_model = create_bikes_model()
+    current_date = datetime.now()
+    new_bike_entry = {"$set": {"model": bike_model, "date_of_training": current_date}}
+    model_info = eureka_collection.update_one({"indicator": "dublin_bikes"}, new_bike_entry)
+    print("Bikes Model saved to DB")
+    print(model_info)
 
 @app.route("/get_pedestrian_predictions", methods=['GET'])
 def load_pedestrian_model():
@@ -35,10 +43,19 @@ def load_pedestrian_model():
     pred = get_pedestrian_predictions(ped_model['model'])
     return ''.join(str(p) for p in pred)
     
+@app.route("/get_bike_predictions", methods=['GET'])
+def load_bike_model():
+    bike_pred_model = eureka_collection.find_one({"indicator": "dublin_bikes"})
+
+    pred = get_bike_predictions(bike_pred_model['model'])
+    return str(pred)
+
 
 if __name__ == "__main__":
     #sched.add_job(save_pedestrian_model, 'cron', hour='12')
-    sched.add_job(save_pedestrian_model, 'cron', minute='*')
-    sched.start()
+    # sched.add_job(save_pedestrian_model, 'cron', minute='*')
+    # sched.start()
+    # sched.add_job(save_bike_model, 'cron', minute='20')
+    # sched.start()
     print('Scheduler Running...')
     app.run()
