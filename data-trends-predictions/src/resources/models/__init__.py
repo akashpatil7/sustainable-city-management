62ffb5458afa8e38b42a4f5c49f0f29ff79e5890
aryanveer <aryanveer310@gmail.com>
2022-03-31 16:19:15 +0100

    fixed errors in bikes model

--- data-trends-predictions/src/resources/models/__init__.py
+++ data-trends-predictions/src/resources/models/__init__.py
@@ -1,13 +1,14 @@
 from flask_restful import Resource
 from .model_pedestrians import PedestrianModel
 from .model_aqi import AqiModel
+from .model_bikes import BikesModel
 
 class Models(Resource):
     def __init__(self, **kwargs):
         self.db = kwargs['db']
         self.pedestrian_model = PedestrianModel(self.db)
         self.aqi_model = AqiModel(self.db)
-        #self.bikes_model = Bikes_model(self.db)
+        self.bikes_model = BikesModel(self.db)
         
 
     def get(self, data_indicator, action):
@@ -15,3 +16,5 @@ class Models(Resource):
             return self.pedestrian_model.perform_action(action)
         if data_indicator == 'aqi':
             return self.aqi_model.perform_action(action)
+        if data_indicator == 'bikes':
+            return self.bikes_model.perform_action(action)
