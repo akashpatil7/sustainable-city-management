e65e428f170fcdd731c7adc33a97b6d79d49997a
Ciara Gilsenan <ciaragil98@gmail.com>
2022-04-01 22:38:17 +0100

    added bus model

--- data-trends-predictions/src/resources/models/__init__.py
+++ data-trends-predictions/src/resources/models/__init__.py
@@ -1,6 +1,7 @@
 from flask_restful import Resource
 from .model_pedestrians import PedestrianModel
 from .model_aqi import AqiModel
+from .model_bus import BusModel
 
 class Models(Resource):
     def __init__(self, **kwargs):
@@ -8,6 +9,7 @@ class Models(Resource):
         self.pedestrian_model = PedestrianModel(self.db)
         self.aqi_model = AqiModel(self.db)
         #self.bikes_model = Bikes_model(self.db)
+        self.bus_model = BusModel(self.db)
         
 
     def get(self, data_indicator, action):
@@ -15,3 +17,5 @@ class Models(Resource):
             return self.pedestrian_model.perform_action(action)
         if data_indicator == 'aqi':
             return self.aqi_model.perform_action(action)
+        if data_indicator == 'bus':
+            return self.bus_model.perform_action(action)
